876a496b82692eb306e2b4fc4c19104fd3ae5b3c
Gireeshkumar Ediga <egireeshkumar99@gmail.com>
2021-12-31 13:14:51 +0530

    code updated

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Userdetails  from './components/Userdetails';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>user details</h1>
+      <Userdetails />
+    </div>
+  );
+}
+
+export default App;
