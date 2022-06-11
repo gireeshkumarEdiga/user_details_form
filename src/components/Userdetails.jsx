79185ab9d05ba09502f302250f580028f9ed1c2e
Gireeshkumar Ediga <egireeshkumar99@gmail.com>
2022-06-11 18:36:58 +0530

    code

--- src/components/Userdetails.jsx
+++ src/components/Userdetails.jsx
@@ -1,288 +1,149 @@
-import { useState, useRef, useEffect } from "react";
+import React from "react";
 
-const inistate = {
+import {useState, useRef, useEffect} from "react";
+
+const initstate = {
     name : "",
     age : "",
     address : "",
     department : "",
     salary : "",
-    ismarried : "",
-    profilepic : "", 
+    isMarried : "",
+    profilepic : "",
 }
-
 export const Userdetails = () => {
-    const [ form, setForm ] = useState(inistate);
-    const [ details, setDetails] = useState([])
 
+    const [ form, setForm] = useState(initstate);
+    const [details, setDetails] = useState([]);
     const fileref = useRef();
+
 
     useEffect(() => {
         getDetails();
     },[])
-
-    const handleChange = (e) => {
-        let { name, value,checked,type } = e.target
-        value = type === "checkbox" ? checked : value;
-        setForm((prev) => ({...prev,[name]:value}));
-    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(form);
     }
 
+    const handleChange = (e) => {
+        let {name,value,type,checked} = e.target;
+        value = type === "checkbox" ? checked : value;
+        setForm((prev) => ({...prev,[name]:value}))
+    }
+
     const postData = () => {
+
         const payload = {
-            name:form.name,
-            age:form.age,
-            address:form.address,
-            salary:form.salary,
-            department:form.department,
-            ismarried : form.ismarried,
+            name : form.name,
+            age : form.age,
+            address : form.address,
+            salary : form.salary,
+            department : form.department,
+            isMarried : form.isMarried,
+            
+            status: false,
+        };
 
-
-            status:false,
-        }
         fetch("http://localhost:3005/users",{
-            method:"POST",
+            method : "POST",
             body : JSON.stringify(payload),
-            headers: {
-                "content-type":"application/json",
-            }
-        }).then(()=> {
+            headers : {
+                "content-type" : "application/json",
+            },
+        })
+        .then(() => {
             getDetails();
             setForm("")
         })
     }
 
     const getDetails = () => {
-        fetch("https://localhost:3005/users")
-        .then(d => d.json())
+        fetch("http://localhost:3005/users")
+        .then((d) => d.json())
         .then((res) => {
             setDetails(res)
-        });
+        })
     }
 
-    const {name,age,address,salary,ismarried,department} = form;
+    const handleClickDelete = (i) => {
+        let result = details.filter((user,index) => index!==i)
+        setDetails(result);
+    }
+
+    const {name,age,address,salary,isMarried,department}=form;
 
     return (
-        <>
         <div id="flex">
             <form onSubmit={handleSubmit}>
-              <label>Name</label>
-              <br />
-              <input name="name" value={name} onChange={handleChange} type="text" placeholder="Enter your name"/>  
-              <br />
-              <br />
-              <label>Age</label>
-              <br />
-              <input name="age" value={age} onChange={handleChange} type="number" placeholder="Enter your age"/>
-              <br />
-              <br />
-              <label>Address</label> 
-              <br />
-              <input name="address" value={address} onChange={handleChange} type="text" placeholder="Enter your address"/>   
-              <br />
-              <br />
-              <select name="department" value={department} onChange={handleChange} >department
-                  <option value="ECE">ECE</option>
-                  <option value="EEE">EEE</option>
-                  <option value="CSE">CSE</option>
-                  <option value="IT">IT</option>
-              </select>
-              <br />
-              <br />
-              <label>salary</label>
-              <br />
-              <input name="salary" value={salary} onChange={handleChange} type="number" placeholder="Enter your salary"/>
-              <br />
-              <br />  
-              <label>ismarried</label>
-              <br />
-              <input name="ismarried" value={ismarried} onChange={handleChange} type="checkbox" placeholder="checkbox"/>  
-              <br />
-              <br />
-              <label>Profile pic</label>
-              <br />
-              <input type="file" ref={fileref}/>  
-              <br />
-              <br />
-              <input onClick={postData} id="submitbtn" type="submit" />  
+                <label>Name</label>
+                <br/>
+                <input type="text" placeholder="eneter the name" value={name} name="name" onChange={handleChange} />
+                <br/>
+                <bt/>
+                <label>Age</label>
+                <br />
+                <input type="number" placeholder="eneter your age" value={age} name="age" onChange={handleChange} />
+                <br />
+                <br />
+                <label>Address</label>
+                <br />
+                <input type="text" placeholder="eneter your address" value={address} name="address" onChange={handleChange} />
+                <br />
+                <br />
+                <label>Department</label>
+                <br />
+                <select name="department" value={department} onChange={handleChange} >
+                    <option value="ECE">ECE</option>
+                    <option value="EEE">EEE</option>
+                    <option value="CSE">CSE</option>
+                    <option value="MECH">MECH</option>
+                    <option value="CIVIL">CIVIL</option>
+                </select>
+                <br />
+                <br />
+                <label>Salary</label>
+                <br />
+                <input type="number" placeholder="eneter your salary" value={salary} name="salary" onChange={handleChange} />
+                <br />
+                <br />
+                <label>isMarried</label>
+                <br />
+                <input type="checkbox" checked={isMarried} name="isMarried" onChange={handleChange} />
+                <br />
+                <br />
+                <label>Profile Pic</label>
+                <br />
+                <input type="file" ref={fileref}/>
+                <br />
+                <br />
+                <input type="button" id="submitbtn" onClick={postData} />
             </form>
-
             <table>
                 <tr>
-                    <th>Name</th>
-                    <th>Age</th>
-                    <th>Address</th>
-                    <th>salary</th>
-                    <th>Department</th>
-                    <th>married status</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Address</th>
+                <th>Salary</th>
+                <th>Department</th>
+                <th>Married Status</th>
+                <th>Operation</th>
                 </tr>
-
-                {details.map((e,i)=>
-                    <tr key={i}>
-                        <td>{e.name}</td>
-                        <td>{e.age}</td>
-                        <td>{e.address}</td>
-                        <td>{e.salary}</td>
-                        <td>{e.department}</td>
-                        <td>{e.ismarried}</td>
-                    </tr>
+                {details.map((e,i) => 
+                <tr key={i}>
+                    <td>{e.name}</td>
+                    <td>{e.age}</td>
+                    <td>{e.address}</td> 
+                    <td>{e.salary}</td> 
+                    <td>{e.department}</td> 
+                    <td>{e.isMarried}</td>
+                    <button onClick={() => handleClickDelete(i)}>Delete</button>
+                </tr>
                 )}
             </table>
         </div>
-        </>
     )
 }
 
 export default Userdetails;
-
-
-
-
-
-// import { useState,useRef, useEffect} from "react"
-
-// const initstate ={
-//     name:"",
-//     age :"",
-//     address:"",
-//     department:"",
-//     salary:"",
-//     isMarried:"",
-//     profilepic:"",
-// }
-
-// export const Userdetails =()=>{
-//     const [form,setForm]=useState(initstate);
-//     const [details,setDetails]=useState([])
-   
-//     const fileref = useRef();
-
-//     useEffect(()=>{
-//         getDetails();
-
-//     },[])
-
-//     const handleChange =(e)=>{
-//         let {name,value,checked,type} =e.target
-//         value=type==="checkbox" ? checked : value;
-//         setForm((prev)=>({...prev,[name]:value}));
-//     }
-
-//     const handleSubmit =(e)=>{
-//         e.preventDefault();
-//         console.log(form);
-//     }
-
-//     const postData=()=>{
-//         const payload={
-//             name:form.name,
-//             age:form.age,
-//             address:form.address,
-//             salary:form.salary,
-//             department:form.department,
-//             isMarried:form.isMarried,
-
-
-//             status:false,
-//         };
-//         fetch("http://localhost:3005/users",{
-//             method:"POST",
-//             body:JSON.stringify(payload),
-//             headers:{
-//                 "content-type":"application/json",
-//             },
-//         }).then(()=>{
-//             getDetails();
-//             setForm("")
-//         })
-//     }
-
-//     const getDetails =()=>{
-//          fetch("http://localhost:3005/users")
-//     .then(d=>d.json())
-//     .then((res)=>{
-//         setDetails(res)});
-//     }
-
-//     const {name,age,address,salary,isMarried,department}=form;
-
-
-//     return (
-//         <>
-//         <div id="flex">
-//         <form  onSubmit={handleSubmit}>
-//             <label>Name</label>
-//             <br />
-//             <input  name="name" value={name} onChange={handleChange} type="text" placeholder="Enter Your Name" />
-//             <br />
-//             <br />
-//             <label>Age</label>
-//             <br />
-//             <input name="age" value={age} onChange={handleChange} type="number" placeholder="Enter your age" />
-//             <br />
-//             <br />
-//             <label>Address</label>
-//             <br />
-//             <input name="address" value ={address} onChange={handleChange} type="text" placeholder="Enter your address" />
-//             <br />
-//             <br />
-//             <select name="department" value={department} onChange={handleChange}>Department
-//                 <option value="ECE">ECE</option>
-//                 <option value="EEE">EEE</option>
-//                 <option value="CSE">CSE</option>
-//                 <option value="IT">IT</option>
-//             </select>
-//             <br />
-//             <br />
-//             <label>Salary</label>
-//             <br />
-//             <input name="salary" value={salary} onChange={handleChange}  type="number" placeholder="salary" />
-//             <br />
-//             <br />
-//             <label>isMarried</label>
-//             <br />
-//             <input name="isMarried" checked={isMarried} onChange={handleChange} type="checkbox"  />
-//             <br />
-//             <br />
-//             <label>Profile Pic</label>
-//             <br />
-//             <input type="file" ref={fileref}  />
-//             <br />
-//             <br />
-//             <input onClick={postData} id="submitbtn" type="submit" />
-//         </form>
-//         <table>
-//   <tr>
-//     <th>Name</th>
-//     <th>Age</th>
-//     <th>Address</th>
-//     <th>Salary</th>
-//     <th>Department</th>
-//     <th>Married Status</th>
-//   </tr>
-  
-//   {details.map((e,i)=>
-//         <tr key ={i} > 
-//             <td>{e.name}</td>
-//             <td>{e.age}</td>
-//             <td>{e.address}</td> 
-//             <td>{e.salary}</td> 
-//             <td>{e.department}</td> 
-//             <td>{e.isMarried}</td>
-//         </tr>)}
-
-  
- 
-// </table>
-        
-
-//              </div>
-//         </>
-//     )
-    
-// }
-
-// export default Userdetails;
